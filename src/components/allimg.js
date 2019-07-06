71cad70c4dc6abf56153120e18eb9c60fd02109e
Shantina <dinadamjanovik@gmail.com>
2019-07-06 02:51:56 +0200

    added gallery

--- src/components/allimg.js
+++ src/components/allimg.js
@@ -1,112 +1,293 @@
-let allimg = [
+const IMAGES = [
   {
-    id: 1,
-    number: 1,
+    src: "./images/allimg/1.jpg",
+    thumbnail: "./images/allimg/1.jpg",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 174,
+    caption: "Shanti Outside Living Room",
   },
   {
-    id: 2,
-    number: 2,
+    src: "./images/allimg/2.jpg",
+    thumbnail: "./images/allimg/2.jpg",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 212,
+    caption: "Shanti Outside Living Room",
+  },
+
+  {
+    src: "./images/allimg/3.jpg",
+    thumbnail: "./images/allimg/3.jpg",
+    caption: "Shanti Living Room",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 212,
   },
   {
-    id: 3,
-    number: 3,
+    src: "./images/allimg/4.jpg",
+    thumbnail: "./images/allimg/4.jpg",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 174,
+    caption: "Shanti Living Room",
   },
   {
-    id: 4,
-    number: 4,
+    src: "./images/allimg/5.jpg",
+    thumbnail: "./images/allimg/5.jpg",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 212,
+    // tags: [
+    //   { value: "Ocean", title: "Ocean" },
+    //   { value: "People", title: "People" },
+    // ],
+    caption: "Shanti Awards",
+  },
+
+  {
+    src: "./images/allimg/6.jpg",
+    thumbnail: "./images/allimg/6.jpg",
+    caption: "Shanti 8 bed dorm",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 212,
   },
   {
-    id: 5,
-    number: 5,
+    src: "./images/allimg/7.jpg",
+    thumbnail: "./images/allimg/7.jpg",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 174,
+    caption: "Shanti 8 bed dorm",
   },
   {
-    id: 6,
-    number: 6,
+    src: "./images/allimg/8.jpg",
+    thumbnail: "./images/allimg/8.jpg",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 212,
+    // tags: [
+    //   { value: "Ocean", title: "Ocean" },
+    //   { value: "People", title: "People" },
+    // ],
+    caption: "Shanti 8 bed dorm",
+  },
+
+  {
+    src: "./images/allimg/9.jpg",
+    thumbnail: "./images/allimg/9.jpg",
+    caption: "Shanti 8 bed dorm",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 212,
   },
   {
-    id: 7,
-    number: 7,
+    src: "./images/allimg/10.jpg",
+    thumbnail: "./images/allimg/10.jpg",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 174,
+    caption: "Shanti 8 bed dorm",
   },
   {
-    id: 8,
-    number: 8,
+    src: "./images/allimg/11.jpg",
+    thumbnail: "./images/allimg/11.jpg",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 212,
+    // tags: [
+    //   { value: "Ocean", title: "Ocean" },
+    //   { value: "People", title: "People" },
+    // ],
+    caption: "Shanti 4 bed dorm- The Orange",
+  },
+
+  {
+    src: "./images/allimg/12.jpg",
+    thumbnail: "./images/allimg/12.jpg",
+    caption: "Shanti 4 bed dorm- The Orange",
+    // thumbnailWidth: 320,
+    // thumbnailHeight: 212,
   },
   {
-    id: 9,
-    number: 9,
+    src: "./images/allimg/13.jpg",
+    thumbnail: "./images/allimg/13.jpg",
+    caption: "Shanti 4 bed dorm- The Orange",
   },
   {
-    id: 10,
-    number: 10,
+    src: "./images/allimg/14.jpg",
+    thumbnail: "./images/allimg/14.jpg",
+    caption: "Shanti 4 bed dorm- The Orange",
   },
   {
-    id: 11,
-    number: 11,
+    src: "./images/allimg/15.jpg",
+    thumbnail: "./images/allimg/15.jpg",
+    caption: "Shanti 4 bed dorm- The Orange",
   },
   {
-    id: 12,
-    number: 12,
+    src: "./images/allimg/16.jpg",
+    thumbnail: "./images/allimg/16.jpg",
+    caption: "Shanti 4 bed dorm- The Green",
   },
   {
-    id: 13,
-    number: 13,
+    src: "./images/allimg/17.jpg",
+    thumbnail: "./images/allimg/17.jpg",
+    caption: "Shanti 4 bed dorm- The Yellow",
   },
   {
-    id: 14,
-    number: 14,
+    src: "./images/allimg/18.jpg",
+    thumbnail: "./images/allimg/18.jpg",
+    caption: "Shanti Private Double Room",
   },
   {
-    id: 15,
-    number: 15,
+    src: "./images/allimg/19.jpg",
+    thumbnail: "./images/allimg/19.jpg",
+    caption: "Shanti Private Double Room",
   },
   {
-    id: 16,
-    number: 16,
+    src: "./images/allimg/20.jpg",
+    thumbnail: "./images/allimg/20.jpg",
+    caption: "Shanti Kitchen",
   },
   {
-    id: 17,
-    number: 17,
+    src: "./images/allimg/21.jpg",
+    thumbnail: "./images/allimg/21.jpg",
+    caption: "Shanti Kitchen",
   },
   {
-    id: 18,
-    number: 18,
+    src: "./images/allimg/22.jpg",
+    thumbnail: "./images/allimg/22.jpg",
+    caption: "Shanti Yard",
   },
   {
-    id: 19,
-    number: 19,
+    src: "./images/allimg/23.jpg",
+    thumbnail: "./images/allimg/23.jpg",
+    caption: "Shanti Yard",
   },
   {
-    id: 20,
-    number: 20,
+    src: "./images/allimg/24.jpg",
+    thumbnail: "./images/allimg/24.jpg",
+    caption: "Shanti Yard",
   },
   {
-    id: 21,
-    number: 21,
+    src: "./images/allimg/25.jpg",
+    thumbnail: "./images/allimg/25.jpg",
+    caption: "Shanti Shared Bathroom",
+  },
+
+  {
+    src: "./images/allimg/26.jpg",
+    thumbnail: "./images/allimg/26.jpg",
+    caption: "Shanti Shared Bathroom",
   },
   {
-    id: 22,
-    number: 22,
-  },
-  {
-    id: 23,
-    number: 23,
-  },
-  {
-    id: 24,
-    number: 24,
-  },
-  {
-    id: 25,
-    number: 25,
-  },
-  {
-    id: 26,
-    number: 26,
-  },
-  {
-    id: 27,
-    number: 27,
+    src: "./images/allimg/27.jpg",
+    thumbnail: "./images/allimg/27.jpg",
+    caption: "Shanti Hostel",
   },
 ]
 
-export default allimg
+export default IMAGES
+
+// let allimg = [
+//   {
+//     id: 1,
+//     number: 1,
+//   },
+//   {
+//     id: 2,
+//     number: 2,
+//   },
+//   {
+//     id: 3,
+//     number: 3,
+//   },
+//   {
+//     id: 4,
+//     number: 4,
+//   },
+//   {
+//     id: 5,
+//     number: 5,
+//   },
+//   {
+//     id: 6,
+//     number: 6,
+//   },
+//   {
+//     id: 7,
+//     number: 7,
+//   },
+//   {
+//     id: 8,
+//     number: 8,
+//   },
+//   {
+//     id: 9,
+//     number: 9,
+//   },
+//   {
+//     id: 10,
+//     number: 10,
+//   },
+//   {
+//     id: 11,
+//     number: 11,
+//   },
+//   {
+//     id: 12,
+//     number: 12,
+//   },
+//   {
+//     id: 13,
+//     number: 13,
+//   },
+//   {
+//     id: 14,
+//     number: 14,
+//   },
+//   {
+//     id: 15,
+//     number: 15,
+//   },
+//   {
+//     id: 16,
+//     number: 16,
+//   },
+//   {
+//     id: 17,
+//     number: 17,
+//   },
+//   {
+//     id: 18,
+//     number: 18,
+//   },
+//   {
+//     id: 19,
+//     number: 19,
+//   },
+//   {
+//     id: 20,
+//     number: 20,
+//   },
+//   {
+//     id: 21,
+//     number: 21,
+//   },
+//   {
+//     id: 22,
+//     number: 22,
+//   },
+//   {
+//     id: 23,
+//     number: 23,
+//   },
+//   {
+//     id: 24,
+//     number: 24,
+//   },
+//   {
+//     id: 25,
+//     number: 25,
+//   },
+//   {
+//     id: 26,
+//     number: 26,
+//   },
+//   {
+//     id: 27,
+//     number: 27,
+//   },
+// ]
+
+// export default allimg
