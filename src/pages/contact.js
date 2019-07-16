04aa0d842da10f534df0d0637f642f72390597cd
Shantina <dinadamjanovik@gmail.com>
2019-07-16 19:59:19 +0200

    added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,16 @@
-import React from "react"
-import Layout from "../components/layout"
-import Head from "../components/head"
-import contactStyles from "./contact.module.scss"
-import Staff from "../images/staff.jpg"
-import Iframe from "react-iframe"
+import React from "react";
+import Layout from "../components/layout";
+import Head from "../components/head";
+import contactStyles from "./contact.module.scss";
+import Staff from "../images/staff.jpg";
+import Iframe from "react-iframe";
 // import Phone from "../phone.png"
 
 import {
   FaPhoneSquare,
   FaSearchLocation,
   FaLocationArrow,
-} from "react-icons/fa"
+} from "react-icons/fa";
 // src={require(`../src/images/covers/${this.state.details.cover}.jpg`)}
 const ContactPage = () => {
   return (
@@ -36,12 +36,13 @@ const ContactPage = () => {
         </div>
         <Iframe
           url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2965.2592849614466!2d21.442481032641428!3d41.994710654246944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x309377b0c6a08aa7!2z0KjQsNC90YLQuCDQpdC-0YHRgtC10LsgMQ!5e0!3m2!1sen!2smk!4v1562017120321!5m2!1sen!2smk"
-          width="900"
+          width="1200px"
           height="450"
           id="myId"
           className="myClassname"
-          display="initial"
+          display="block"
           position="relative"
+          styles={{ margin: "0 auto" }}
           className={contactStyles.map}
         />
         <img className={contactStyles.staff} src={Staff} alt="staff" />
@@ -52,7 +53,7 @@ const ContactPage = () => {
         </p>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
